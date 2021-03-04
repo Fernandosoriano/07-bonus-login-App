149b19df83997bdb6aaf3bff5f96808ffb6bcda2
Fernandosoriano <fersoriano@ciencias.unam.mx>
2021-03-04 12:51:28 -0600

    guardado de token, y a,erta con swwet alert

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { AuthService } from 'src/app/services/auth.service';
+// import swal from 'sweetalert';
+// import Swal from 'sweetalert2';
+import swal from 'sweetalert';
+// import * as _swal from 'sweetalert';
+// import { SweetAlert } from 'sweetalert/typings/core';
 
+// const swal: SweetAlert = _swal as any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +19,7 @@ export class LoginComponent implements OnInit {
   usuario: UsuarioModel;
   
 
-  constructor() { 
+  constructor(private auth: AuthService) { 
     this.usuario = new UsuarioModel(); // inicializacion
   }
 
@@ -23,10 +30,29 @@ export class LoginComponent implements OnInit {
     if(form.invalid){
       return;
     }
-    console.log('imprimi si el formulario es valido')
-    console.log('formulario enviado');
-    console.log(this.usuario);
-    console.log(form)
+    swal("Espere por favor...");
+
+    // Swal.fire({
+    //   allowOutsideClick:false,
+    // })
+    // Swal.fire({
+    //   allowOutsideClick: false,
+    //   type: 'info',
+    //   text: 'Espere por favor...'
+    // });
+
+    this.auth.login(this.usuario).subscribe(
+      resp =>{
+        console.log(resp);
+      }, (err) => {
+        console.log(err.error.error.message)
+
+      }
+    )
+    // console.log('imprimi si el formulario es valido')
+    // console.log('formulario enviado');
+    // console.log(this.usuario);
+    // console.log(form)
     
   }
 
